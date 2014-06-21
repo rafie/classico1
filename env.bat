07fd9edb66ec2e5dfcf8510f4c2744f400884bd6
Raffapen <rafi.einstein@gmail.com>
2014-06-21 15:36:04 +0300

    fixed env.bat

--- env.bat
+++ env.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set RROOT=d:\lab\classico
+set RROOT=%~dp0
 set GEM_HOME=%RROOT%\Gems
 set PATH=%RROOT%\Gems\bin;%RROOT%\Ruby\bin;%PATH%
 set RUBYLIB=%RROOT%\Classico;%RROOT%\Confetti;%RUBYLIB%
