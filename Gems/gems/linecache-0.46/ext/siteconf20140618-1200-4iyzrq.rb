0e6577d2aaa9cd4afecfbe054f36d009828581c4
rafie <rafie@avaya.com>
2014-06-26 11:27:30 +0300

    added some gems

---
+++ Gems/gems/linecache-0.46/ext/siteconf20140618-1200-4iyzrq.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20140618-1200-1n08mit"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
