95814da4526e5255a34573ed2c27a7e0d03f410a
raffapen <rafi.einstein@gmail.com>
2014-06-29 23:59:01 +0300

    bundle update

--- Gems/gems/byebug-3.0.0/lib/byebug/version.rb
+++
@@ -1,3 +0,0 @@
-module Byebug
-  VERSION = '3.0.0'
-end
--- Gems/gems/byebug-3.0.0/test/timeout_test.rb
+++
@@ -1,9 +0,0 @@
-class TestTimeout < MiniTest::Spec
-
-  it 'call to "Timeout::timeout" after "Byebug.start" does not raise' do
-    Byebug.start do
-      Timeout::timeout(60) {}
-    end
-  end
-
-end
