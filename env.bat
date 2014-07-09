373fed7ac44cbe6a9989abaf5cafe3237a0c192e
rafie <rafie@avaya.com>
2014-07-09 20:38:41 +0300

    database logic

--- env.bat
+++ env.bat
@@ -1,6 +1,7 @@
 @echo off
 
 set RROOT=%~dp0
+set R1ROOT=%RROOT%\..
 set GEM_HOME=%RROOT%\Gems
-set PATH=%RROOT%\Gems\bin;%RROOT%\Ruby\bin;%PATH%
-set RUBYLIB=%RROOT%\Classico;%RROOT%\Confetti;%RUBYLIB%
+set PATH=%RROOT%\Gems\bin;%RROOT%\Ruby\bin;%R1ROOT%\confetti1\Confetti\bin;%PATH%
+set RUBYLIB=%RROOT%\Classico;%R1ROOT%\confetti1\Confetti;%RROOT%\nexp;%RUBYLIB%
