17bf14546bc31df9c04b052c45dc0705d8e2e633
rafie <rafie@avaya.com>
2014-08-20 20:46:44 +0300

    removed Bento

--- Classico/Bento.rb
+++
@@ -1,7 +0,0 @@
-
-require_relative 'Bento/lib/Class'
-require_relative 'Bento/lib/System'
-require_relative 'Bento/lib/ClearCASE'
-require_relative 'Bento/lib/DB'
-require_relative 'Bento/lib/Diag'
-require_relative 'Bento/lib/Misc'
